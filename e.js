62d0f57e606584d360f048e6de7749ba963b28ae
drakejin <drake-jin@users.noreply.github.com>
2017-05-28 03:50:51 +0900

    Update e.js

--- e.js
+++ e.js
@@ -1,13 +1,24 @@
-$("#comment").html(
-`
-    
+$("#comment").html(`
     <script>
-    function yo472(){
-       console.log("아따 잘지내는가?");
-       yield;
-       console.log("나는 요새 존나 면접 보러 다닌다네");
+    function* yo472(){
+       talk = [
+         '아따 우리 엠띠엔띠 전 회원님은 잘계시나?  (한번 더 눌러)',
+         '기왕 스크립팅 장난치는거 재밌게 쳐볼라칸다잉',
+         '또 이건 너만 보라고 이러는 거양ㅋㅋㅋ',
+         '공든장난 뀨 >   <',
+         '요새취업잘안된다기보다그냥',
+         '내가 지금 이도 저도 아닌것들을 하는거 같아서 자책감에 힘들드라',
+         '넌 요즘 뭐 하고 지내냐',
+         'ㅋㅋㅋㅋ카톡보다는 이걸로 말하는게 더 반가울거 같아서 공들여본다.'
+       ];
+       let index = 0;
+       while(index <= talk.length){
+           yield talk[index];
+           index++;
+       }
     }
+    talking = yo472();
     </script>
-    <a href="javascript:yo472()"> 악성스크립트 아니야 믿어바 </a>
+    <a href="javascript:console.log(talking.next().value)"> 이거 누르고 콘솔창 확인하기 </a>
 `
 );
