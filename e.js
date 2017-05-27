867ed0c158041258400421fb1488c7763574d3bc
drakejin <drake-jin@users.noreply.github.com>
2017-05-28 03:36:56 +0900

    Update e.js

--- e.js
+++ e.js
@@ -1,7 +1,7 @@
 $("#comment").html(`
     <a href="javascript:yo472()"> 악성스크립트 아니야 믿어바 </a>
     <script>
-    yo472()=>{
+    function yo472(){
        console.log("아따 잘지내는가?");
        yield;
        console.log("나는 요새 존나 면접 보러 다닌다네");
