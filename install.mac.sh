eb5b1345fe7a52ac9a7dbe88125f9ad68d3abfaf
drake-jin <dydwls121200@gmail.com>
2017-12-13 07:07:39 +0900

    init project

---
+++ install.mac.sh
@@ -0,0 +1,14 @@
+brew install ruby
+
+sudo xcode-select --install # isntall ruby-dev
+sudo xcodebuild -license # isntall ruby-dev
+
+sudo gem install jekyll
+sudo gem install bundle
+
+brew install libxml2
+
+bundle install --path vendor/bundle # gitignore에 추가해준다.
+
+npm install 
+npm install -g gulp gulp-cli
